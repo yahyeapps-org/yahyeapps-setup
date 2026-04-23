7cef8464e2c28d45ebc494f1e4a965e68786ee37
yahyeapps2022 <yahyeapps2022@gmail.com>
2026-04-23 17:44:17 +0500

    deploy

--- server-setup/server-config/server-config.sh
+++ server-setup/server-config/server-config.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
  
-echo "$MYSUDOPASS" | sudo -S -v
+echo "$MYecho "$MYSUDOPASS" | sudo -S COMMANDPASS" | suddo -S -v
 
 while true; do
-  echo "$MYSUDOPASS" | sudo -S -v
+  echo "$MYecho "$MYSUDOPASS" | sudo -S COMMANDPASS" | suddo -S -v
   sleep 60
 done &
 
 
-echo "soodhoow yahye" | sudo tee /etc/motd
+echo "soodhoow yahye" | echo "$MYSUDOPASS" | sudo -S COMMAND tee /etc/motd
 
-sudo apt update -y 
-sudo apt-get update -y 
+echo "$MYSUDOPASS" | sudo -S COMMAND apt update -y 
+echo "$MYSUDOPASS" | sudo -S COMMAND apt-get update -y 
 
 # install ufw 
-sudo apt install ufw -y
-sudo cp ./server-setup/server-config/update-cloudflare-ufw.sh /usr/local/bin/ 
-sudo chmod +x /usr/local/bin/update-cloudflare-ufw.sh
-echo "0 0 * * * /usr/local/bin/update-cloudflare-ufw.sh >/dev/null 2>&1" | sudo crontab -u root -
+echo "$MYSUDOPASS" | sudo -S COMMAND apt install ufw -y
+echo "$MYSUDOPASS" | sudo -S COMMAND cp ~/server-setup/server-config/update-cloudflare-ufw.sh /usr/local/bin/ 
+echo "$MYSUDOPASS" | sudo -S COMMAND chmod +x /usr/local/bin/update-cloudflare-ufw.sh
+echo "0 0 * * * /usr/local/bin/update-cloudflare-ufw.sh >/dev/null 2>&1" | echo "$MYSUDOPASS" | sudo -S COMMAND crontab -u root -
 
 
 bash /usr/local/bin/update-cloudflare-ufw.sh 
 
 # install cron
-sudo apt install cron -y
+echo "$MYSUDOPASS" | sudo -S COMMAND apt install cron -y
 #install git
-sudo apt install git -y
+echo "$MYSUDOPASS" | sudo -S COMMAND apt install git -y
 git config --global user.name "yahyeapps2022"
 git config --global user.email "yahyeapps2022@gmail.com"
 
 #install k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" sh -
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo chown $USER:$USER /etc/rancher/k3s/k3s.yaml
+echo "$MYSUDOPASS" | sudo -S COMMAND chmod 644 /etc/rancher/k3s/k3s.yaml
+echo "$MYSUDOPASS" | sudo -S COMMAND chown $USER:$USER /etc/rancher/k3s/k3s.yaml
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc
 source ~/.bashrc
@@ -49,10 +49,10 @@ echo "🚀 Optimizing WebSockets for 10,000+ connections..."
 echo "🔧 Updating kernel parameters..."
 
 # Remove old block if exists
-sudo sed -i '/# WEB SOCKET TUNING START/,/# WEB SOCKET TUNING END/d' /etc/sysctl.conf
+echo "$MYSUDOPASS" | sudo -S COMMAND sed -i '/# WEB SOCKET TUNING START/,/# WEB SOCKET TUNING END/d' /etc/sysctl.conf
 
 # Add new block
-sudo tee -a /etc/sysctl.conf > /dev/null <<EOF
+echo "$MYSUDOPASS" | sudo -S COMMAND tee -a /etc/sysctl.conf > /dev/null <<EOF
 # WEB SOCKET TUNING START
 # Increase pending connections queue
 net.core.somaxconn=65535
@@ -71,15 +71,15 @@ fs.file-max=5000000
 # WEB SOCKET TUNING END
 EOF
 
-sudo sysctl --system
+echo "$MYSUDOPASS" | sudo -S COMMAND sysctl --system
 
 # --- Step 2: File Descriptors and Process Limits ---
 echo "🔧 Updating user limits..."
 
 for file in /etc/security/limits.conf /etc/systemd/system.conf /etc/systemd/user.conf; do
-  sudo sed -i '/# WEB SOCKET TUNING START/,/# WEB SOCKET TUNING END/d' "$file"
+  echo "$MYSUDOPASS" | sudo -S COMMAND sed -i '/# WEB SOCKET TUNING START/,/# WEB SOCKET TUNING END/d' "$file"
 
-  sudo tee -a "$file" > /dev/null <<EOF
+  echo "$MYSUDOPASS" | sudo -S COMMAND tee -a "$file" > /dev/null <<EOF
 # WEB SOCKET TUNING START
 * soft nofile 1000000
 * hard nofile 1000000
@@ -91,5 +91,5 @@ DefaultLimitNPROC=100000
 EOF
 done
 
-sudo systemctl daemon-reexec
+echo "$MYSUDOPASS" | sudo -S COMMAND systemctl daemon-reexec
 echo "✅ WebSocket tuning applied!"
